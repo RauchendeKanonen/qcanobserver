93b5aaf14ce547fd188beef35d40981fc3230b31
netseal <netseal@304f0798-8822-4414-9098-4d000f7c9222>
2009-12-18 12:43:50 +0000

    little fix

--- DeviceLib/linux/pcan/pcanlib.cpp
+++ DeviceLib/linux/pcan/pcanlib.cpp
@@ -89,7 +89,7 @@ int CANDevice::CANDeviceRead(_CANMsg *Msg)
     {
         return 0;
     }
-    Msg->TimeStamp = -1;
+    //Msg->TimeStamp = -1;
     Msg->ID = HWMsg.ID;
     memcpy(Msg->DATA, HWMsg.DATA, 8);
     Msg->LEN = HWMsg.LEN;
